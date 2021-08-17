e3b58b3ccd25fb1064c749fd046a16d207a7c12b
Ryan Hamel <ryan.hamel@microsoft.com>
2021-08-17 17:14:46 -0400

    Support either python2 or 3, and handle new error response if legacy path is incorrectly used

--- specs/execute/cluster-init/files/get_node_id.py
+++ specs/execute/cluster-init/files/get_node_id.py
@@ -10,7 +10,10 @@ EARLIEST_NODEID_JETPACK_7_VERSION = "7.9.0"
 
 def check_output(args):
     with open("/dev/null", "w") as stderr:
-        return check_output_real(args, stderr=stderr)
+        response = check_output_real(args, stderr=stderr)
+        if sys.version < "3.":
+            return response
+        return response.decode()
 
 
 def get_node_id_legacy():
@@ -23,20 +26,16 @@ def get_node_id_legacy():
     query_filter = urllib_quote("(ClusterName===\"" + cluster_name + "\"&&InstanceId===\"" + instance_id + "\")")
     nodes_output = check_output(["curl", "-k", "-u", username + ":" + password, web_server + "/db/Cloud.Node?attr=NodeId&f=" + query_filter + "&format=json"])
     nodes = json.loads(nodes_output)
+    if isinstance(nodes, dict):
+        raise RuntimeError(str(nodes))
     return nodes[0]["NodeId"]
 
     #nodes_output = check_output(["curl", "-k", "-u", username + ":" + password, web_server + "/db/Cloud.Node?attr=NodeId&f=(ClusterName===%22" + cluster_name + "%22%26%26InstanceId%3D%3D%22" + instance_id + "%22)&format=json"])
     
 def get_node_id():
-    jetpack_version = check_output(["jetpack", "-v"]).strip()
     try:
-        current_version = [int(x.split("-")[0]) for x in jetpack_version.split('.')]
-        min_version = [int(x) for x in EARLIEST_NODEID_JETPACK_7_VERSION.split('.')]
-        for i in range(min(len(min_version),len(current_version))):
-            if current_version[i] < min_version[i]:
-                return get_node_id_legacy()
         return check_output(["jetpack", "config", "cyclecloud.node.id"]).strip()
-    except:
+    except Exception:
         return get_node_id_legacy()
 
 node_id = get_node_id()
